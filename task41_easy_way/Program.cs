a7e932394815d0497f13406b0468ffb9f8cf8cf1
Serg_Sytnikov <tlingaal1996@gmail.com>
2022-08-29 01:54:45 +0300

    task41 easy way

---
+++ task41_easy_way/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.WriteLine("Введите количество чисел - элементов анализируемой выборки М: ");
+int amountNumbers = Convert.ToInt32(Console.ReadLine());
+
+int countPositiveNumbers = 0;
+
+for (int i = 1; i <= amountNumbers; i++)
+{
+    Console.WriteLine("Введите число: ");
+    double sampleNumber = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"Осталось ввести чисел: {amountNumbers - i}");
+
+    if (sampleNumber > 0) countPositiveNumbers++;
+}
+
+Console.WriteLine($"Количество положительных чисел в выборке: {countPositiveNumbers}");
