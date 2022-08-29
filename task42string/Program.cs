47ae29868fb430c2a7e17429c7e6d6ad02d8a8db
Serg_Sytnikov <tlingaal1996@gmail.com>
2022-08-29 07:53:32 +0300

    task42 string

---
+++ task42string/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10 */
+
+Console.WriteLine("Введите натуральное число N: ");
+int decimalNumber = Convert.ToInt32(Console.ReadLine());
+
+string binaryNumber = "";
+int N = decimalNumber;
+
+while (N >= 1)
+{
+    if (N % 2 == 1) binaryNumber = "1" + binaryNumber;
+    else binaryNumber = "0" + binaryNumber;
+    N /= 2;
+}
+
+Console.WriteLine($"Число {decimalNumber} в двоичной форме: {binaryNumber}");
